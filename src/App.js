e32495196f5d367219b4d2a4267d0430d34b7752
unknown <elam@nabi.nu>
2020-12-28 23:52:45 +0100

    Working on the navbar, styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Products from "./pages/Products";
+
+import { AnimatePresence } from "framer-motion";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <Router>
+      <AnimatePresence>
+        <Navbar />
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
+        <Route path="/contact" exact component={Contact} />
+        <Route path="/products" exact component={Products} />
+      </AnimatePresence>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { NavLink as Link } from "react-router-dom";
+
+import styled from "styled-components";
+import { IoMdHeartEmpty } from "react-icons/io";
+import { BiBasket } from "react-icons/bi";
+
+const Navbar = () => {
+  return (
+    <Nav>
+      <ul>
+        <li>
+          <NavLink to="/" exact activeClassName="active">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/products" exact activeClassName="active">
+            Products
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" exact activeClassName="active">
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact" exact activeClassName="active">
+            Contact
+          </NavLink>
+        </li>
+        <li>
+          <IoMdHeartEmpty />
+        </li>
+        <li>
+          <BiBasket />
+        </li>
+      </ul>
+    </Nav>
+  );
+};
+
+export default Navbar;
+
+const NavLink = styled(Link)``;
+const Nav = styled.div`
+  margin-top: 20px;
+  ul {
+    display: flex;
+    justify-content: space-evenly;
+    li {
+      list-style: none;
+    }
+
+    li:nth-child(5) {
+      color: #fff;
+      position: absolute;
+      right: 160px;
+    }
+
+    li:nth-child(6) {
+      right: 40px;
+      color: #fff;
+      position: absolute;
+      right: 140px;
+    }
+    a {
+      text-decoration: none;
+      color: #b5b6b8;
+    }
+  }
+
+  .active {
+    color: #fff;
+    text-shadow: -1px -1px 11px rgba(255, 255, 255, 1);
+  }
+`;
